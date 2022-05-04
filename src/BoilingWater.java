98b721b29856750d41d8b2db2edb4e37120a995a
Drew <drewbdoo@gmail.com>
2022-05-04 10:35:19 -0400

    adding NumberComparison.java

--- src/BoilingWater.java
+++ src/BoilingWater.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BoilingWater {
 
     public static void main(String[] args) {
-                  Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     System.out.println("Enter a number greater than 212:");
     double reply = scanner.nextDouble();
     if(reply>=212) {
