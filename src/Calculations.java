ab46d5fc5dfd058308e1038eafa9cc93b15e798d
Drew <drewbdoo@gmail.com>
2022-05-04 10:04:07 -0400

    changing calculations.java

--- src/Calculations.java
+++ src/Calculations.java
@@ -8,13 +8,11 @@ public class Calculations {
         int num1 = scanner.nextInt();
         System.out.println("Enter another number: ");
         int num2 = scanner.nextInt();
-        String n1 = String.valueOf(num1);
-        String n2 = String.valueOf(num2);
-        System.out.println(n1 + " + " + n2 + " = " + (num1 + num2));
-        System.out.println(n1 + " - " + n2 + " = " + (num1 - num2));
-        System.out.println(n1 + " * " + n2 + " = " + (num1 * num2));
-        System.out.println(n1 + " / " + n2 + " = " + (num1 / num2));
-        System.out.println(n1 + " % " + n2 + " = " + (num1 % num2));
+               System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
 
     }
 
