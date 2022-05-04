c916d2fdc0ddb93a21718d58e303fe841df1ef3f
Drew <drewbdoo@gmail.com>
2022-05-04 10:30:36 -0400

    adding BoilingWater.java

--- src/BoilingWater.java
+++ src/BoilingWater.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
+
 // import java.util.Scanner;
 
-// public class BoilingWater {
+public class BoilingWater {
 
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-//         System.out.println("Enter a number greater than 212:");
-//         String reply = scanner.nextLine();
-//         if(reply.equals)
-//     }
-    
-// }
+    public static void main(String[] args) {
+                  Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a number greater than 212:");
+    double reply = scanner.nextDouble();
+    if(reply>=212) {
+        System.out.println("Water is boiling!");
+    }
+    }
+}
--- src/Calculations.java
+++ src/Calculations.java
@@ -5,9 +5,9 @@ public class Calculations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number: ");
-        int num1 = scanner.nextInt();
+        double num1 = scanner.nextDouble();
         System.out.println("Enter another number: ");
-        int num2 = scanner.nextInt();
+        double num2 = scanner.nextDouble();
                System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
         System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
         System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
